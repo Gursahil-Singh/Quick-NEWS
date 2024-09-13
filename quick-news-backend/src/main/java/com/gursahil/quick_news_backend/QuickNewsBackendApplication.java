ce28fa0d3cd9891cb9d8405ade4084a20db62d5e
Gursahil Singh <gursahil4u@gmail.com>
2024-09-13 18:51:42 +0100

    Added Spring Boot

---
+++ quick-news-backend/src/main/java/com/gursahil/quick_news_backend/QuickNewsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gursahil.quick_news_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickNewsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickNewsBackendApplication.class, args);
+	}
+
+}
---
+++ quick-news-backend/src/test/java/com/gursahil/quick_news_backend/QuickNewsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gursahil.quick_news_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickNewsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

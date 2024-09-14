6062ffbc7f9ab04ee01334b1125a26555973acb6
Gursahil Singh <gursahil4u@gmail.com>
2024-09-14 19:28:31 +0100

    Added cross origin and  local host path for frontend

--- quick-news-backend/src/main/java/com/gursahil/quick_news_backend/NewsApiController.java
+++ quick-news-backend/src/main/java/com/gursahil/quick_news_backend/NewsApiController.java
@@ -3,12 +3,14 @@ package com.gursahil.quick_news_backend;
 import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin("http://localhost:5173/")
 public class NewsApiController {
 
     @Autowired

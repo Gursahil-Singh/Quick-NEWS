44ef524ca1e21552cb97230a6ded6c6da267a0f7
Gursahil Singh <gursahil4u@gmail.com>
2024-09-14 19:42:27 +0100

    fixed button position in header

--- quick-news-frontend/src/components/Header.jsx
+++ quick-news-frontend/src/components/Header.jsx
@@ -4,8 +4,8 @@ import Button from './Button'
 export default function Header(props) {
   const{button, buttonState} = props
   return (
-    <div className='cursor-default bg-slate-950 text-white min-h-12 flex justify-between md:justify-center items-center px-4'>
-        <h1 className='sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl font-extrabold tracking-wide'>
+    <div className='bg-slate-950 text-white min-h-12 flex justify-between md:justify-center relative items-center px-4'>
+        <h1 className='cursor-default sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl font-extrabold tracking-wide'>
             Quick<span className='text-sky-700'>NEWS</span>
         </h1>
         <div className='md:absolute md:right-4 text-xs font-semibold border-2 border-solid rounded-full p-1  bg-white text-slate-950'>

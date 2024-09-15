bf687194b904492caf662a5b5503f65652cf8c03
Gursahil Singh <gursahil4u@gmail.com>
2024-09-15 14:08:23 +0100

    Added the code to save or show article link on clicking a button

--- quick-news-frontend/src/components/SummarizedNews.jsx
+++ quick-news-frontend/src/components/SummarizedNews.jsx
@@ -7,6 +7,19 @@ export default function SummarizedNews(props) {
   const[title,setTitle] = useState("")
   const[summary,setSummary] = useState("")
   const[link,setLink] = useState("")
+  const[popup,setPopup] = useState(false)
+  const[saved,setSaved] = useState(false)
+
+  function saveArticle(){
+    setSaved(true)
+  }
+  function showPopup(){
+    setPopup(!popup)
+  }
+  function back(){
+    setPopup(!popup)
+    setSaved(false)
+  }
 
   useEffect(()=>{
     const fetchData = async () => {
@@ -25,7 +38,8 @@ export default function SummarizedNews(props) {
   },[]);
 
   return (
-    <div className= 'h-screen -mt-12 gap-6 p-4 flex flex-col items-center justify-center'>
+    <>
+      {(!popup) && <div className= 'h-screen -mt-12 gap-6 p-4 flex flex-col items-center justify-center'>
         <div className='border-2 border-black border-solid h-auto w-3/4 bg-slate-400 rounded-xl shadow-lg shadow-sky-700'>
           <div className='border border-solid bg-slate-950 text-white rounded-xl p-4 sm:text-lg md:text-xl lg:text-2xl xl:text-3xl font-bold tracking-wide'>
             {title}
@@ -34,16 +48,34 @@ export default function SummarizedNews(props) {
             {summary}
           </div>
           <div className='flex justify-between p-4'>
-            <button className='text-xs font-semibold border-2 border-solid rounded-full bg-white text-slate-950 shadow-md shadow-black'>
+            <button onClick={()=>{showPopup(); saveArticle()}} className='text-xs font-semibold border-2 border-solid rounded-full bg-white text-slate-950 shadow-md shadow-black'>
               Save Article
             </button>
-            <button className='text-xs font-semibold border-2 border-solid rounded-full bg-white text-slate-950 shadow-md shadow-black'>
+            <button onClick={showPopup} className='text-xs font-semibold border-2 border-solid rounded-full bg-white text-slate-950 shadow-md shadow-black'>
               Read Article
             </button>
           </div>
         </div>
         <Button onClick={func} label="Close" />
-    </div>
+    </div>}
+    
+    {(popup) && <div className= 'h-screen -mt-12 gap-6 p-4 flex flex-col items-center justify-center'>
+        <div className='border-2 border-black border-solid h-auto w-3/4 bg-slate-400 rounded-xl shadow-lg shadow-sky-700'>
+          {(!saved) && <div className='sm:text-lg underline flex justify-center items-center md:text-lg lg:text-xl xl:text-2xl font-semibold tracking-wide p-2'>
+            <a href={link} target="_blank" >{link}</a>
+          </div>}
+          {(saved) && <div className='sm:text-lg flex justify-center items-center md:text-lg lg:text-xl xl:text-2xl font-semibold tracking-wide p-2'>
+            Article is saved
+          </div>}
+          <div className='flex justify-center items-center p-4'>
+            <button onClick={back} className='text-xs font-semibold border-2 border-solid rounded-full bg-white text-slate-950 shadow-md shadow-black'>
+              Back
+            </button>
+          </div>
+        </div>
+    </div>}
+    
+    </>
   )
 }
 
